21e87997fe6c4ff900b7cf6145f1ca2ef62c1873
gbantique <gbantique@gmail.com>
2024-01-12 07:24:02 +0800

    added a test code

--- main.ts
+++ main.ts
@@ -1,1 +1,24 @@
-let max7219 = 0
+Brickcell.setup(
+    4,
+    DigitalPin.P16,
+    DigitalPin.P15,
+    DigitalPin.P14,
+    DigitalPin.P13
+)
+basic.forever(function () {
+    Brickcell.scrollText(
+        "Hello world!",
+        50,
+        50
+    )
+    for (let index = 0; index <= 23; index++) {
+        Brickcell.displayCustomCharacter(
+            Brickcell.getCustomCharacterArray(
+                "B00100000,B01000000,B10000110,B10000000,B10000000,B10000110,B01000000,B00100000"
+            ),
+            index,
+            true
+        )
+        basic.pause(200)
+    }
+})
