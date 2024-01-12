aa306e5de4c3d03900574f4044eacdad80357592
gbantique <gbantique@gmail.com>
2024-01-12 08:25:23 +0800

    modified test file display rotation

--- main.ts
+++ main.ts
@@ -1,24 +1,18 @@
 Brickcell.setup(
-    4,
-    DigitalPin.P15,
-    DigitalPin.P14,
-    DigitalPin.P13,
-    DigitalPin.P16
+4,
+DigitalPin.P15,
+DigitalPin.P14,
+DigitalPin.P13,
+DigitalPin.P16
+)
+Brickcell.for_4_in_1_modules(
+rotation_direction.clockwise,
+false
 )
 basic.forever(function () {
     Brickcell.scrollText(
-        "Hello world!",
-        50,
-        50
+    "Hello world!",
+    50,
+    200
     )
-    for (let index = 0; index <= 23; index++) {
-        Brickcell.displayCustomCharacter(
-            Brickcell.getCustomCharacterArray(
-                "B00100000,B01000000,B10000110,B10000000,B10000000,B10000110,B01000000,B00100000"
-            ),
-            index,
-            true
-        )
-        basic.pause(200)
-    }
 })
