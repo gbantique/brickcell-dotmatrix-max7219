e7677ae186cb96c49719cba3ce7979f6e7abbfe7
gbantique <gbantique@gmail.com>
2024-01-12 09:26:19 +0800

    modified test file

--- main.ts
+++ main.ts
@@ -1,1 +1,27 @@
 let dotmatrix8x8 = Brickcell.create()
+dotmatrix8x8.setup(
+dotmatrix8x8,
+DigitalPin.P16,
+DigitalPin.P15,
+DigitalPin.P14,
+DigitalPin.P13
+)
+basic.forever(function () {
+    dotmatrix8x8.scrollText(
+    dotmatrix8x8,
+    50,
+    50
+    )
+    for (let index = 0; index <= 23; index++) {
+        dotmatrix8x8.getCustomCharacterArray(
+        dotmatrix8x8
+        ).displayCustomCharacter(
+        dotmatrix8x8.getCustomCharacterArray(
+        dotmatrix8x8
+        ),
+        index,
+        true
+        )
+        basic.pause(50)
+    }
+})
