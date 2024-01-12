96c5d439d6153551749f730d5a8f582e6d137cad
gbantique <gbantique@gmail.com>
2024-01-12 10:58:22 +0800

    moved main.ts to test.ts

--- test.ts
+++ test.ts
@@ -1,1 +1,17 @@
-// tests go here; this will not be compiled when this package is used as an extension.
+let max7219 = Brickcell.create()
+max7219.setup(
+    1,
+    DigitalPin.P15,
+    DigitalPin.P14,
+    DigitalPin.P13,
+    DigitalPin.P16,
+    max7219_rotation_direction.clockwise,
+    true
+)
+basic.forever(function () {
+    max7219.scrollText(
+        "Brickcell 8x8 Dot Matrix Display",
+        150,
+        300
+    )
+})
